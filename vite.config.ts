c6c908c677d242893928d559efb7d29938e698fc
GHOSTSOLO92 <mr.benrogerson@gmail.com>
2026-03-16 18:38:18 +0000

    Create vite.config.ts for HERO BASTION DEFENSE

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: 'src/main.jsx',
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
+});
